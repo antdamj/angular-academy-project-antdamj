c48ce02ac48095ebd79e4cda6a0d99bf1531feab
antdamj <antonio.damjanovic.8@gmail.com>
2021-07-07 23:40:32 +0200

    full functionality done

--- first/index.js
+++ first/index.js
@@ -1,102 +1,96 @@
-const addReviewForm = document.querySelector("#add-review");
-
-// onload ?
-for (let i = 0; i < localStorage.length; i++) {
-    reviews.push({
-        "id": i,
-        "data": {
-            "text": localStorage.key(i),
-            "score": localStorage.getItem(localStorage.key(i))
-        }
-    });
-}
+const addReviewForm = document.querySelector('#add-review');
 
 const renderData = () => {
-    const reviewsList = document.querySelector("#reviews-list");
-    reviewsList.innerHTML = ""
+	const reviewsList = document.querySelector('#reviews-list');
+	reviewsList.innerHTML = '';
 
-    Object.keys(localStorage).forEach(function(key){
-        let element;
+	const avgRating = document.querySelector('#avg-rating');
+	let avg = 0.0;
 
-        try {
-            element = JSON.parse(localStorage.getItem(key));
-        } catch (e) {
-            console.log("error")
-        };
+	Object.keys(localStorage).forEach(function (key) {
+		let element;
 
-        if (element != null) {
-            const reviewBox = document.createElement("div");
-            reviewBox.classList.add('review-box')
-            reviewBox.classList.add('centered-container')
-            reviewBox.classList.add('col-12')
+		try {
+			element = JSON.parse(localStorage.getItem(key));
+		} catch (e) {
+			console.log('error');
+		}
 
-            const reviewData = document.createElement("div");
-            reviewData.classList.add('col-11');
+		if (element != null) {
+			avg += parseFloat(element.score);
 
-            const reviewText = document.createElement("div");
-            const reviewRating = document.createElement("div");
+			const reviewBox = document.createElement('div');
+			reviewBox.classList.add('review-box');
+			reviewBox.classList.add('centered-container');
+			reviewBox.classList.add('col-12');
 
-            reviewText.textContent = element.text;
-            reviewData.appendChild(reviewText)
-            reviewRating.textContent = element.score + "/5";
-            reviewData.appendChild(reviewRating);
+			const reviewData = document.createElement('div');
+			reviewData.classList.add('col-11');
 
-            const reviewDelete = document.createElement("div");
-            reviewDelete.classList.add('col-1');
-            const reviewDeleteButton = document.createElement("button");
-            reviewDeleteButton.textContent = "-";
-            reviewDeleteButton.onclick = function() {
-                localStorage.removeItem(key);
-                renderData();
-            }
-            reviewDelete.appendChild(reviewDeleteButton);
+			const reviewText = document.createElement('div');
+			const reviewRating = document.createElement('div');
 
-            reviewBox.appendChild(reviewData);
-            reviewBox.appendChild(reviewDelete);
+			reviewText.textContent = element.text;
+			reviewData.appendChild(reviewText);
+			reviewRating.textContent = element.score + '/5';
+			reviewData.appendChild(reviewRating);
 
-            // add list item to list
-            reviewsList.appendChild(reviewBox);
-        }
-    });
+			const reviewDelete = document.createElement('div');
+			reviewDelete.classList.add('col-1');
+			const reviewDeleteButton = document.createElement('button');
+			reviewDeleteButton.textContent = '-';
+			reviewDeleteButton.onclick = function () {
+				localStorage.removeItem(key);
+				renderData();
+			};
+			reviewDelete.appendChild(reviewDeleteButton);
 
-    // clear form fields
-    addReviewForm.reset();
-}
+			reviewBox.appendChild(reviewData);
+			reviewBox.appendChild(reviewDelete);
 
-addReviewForm.addEventListener("submit", (event) => {
-    
-    // dont reload on sumbit
-    event.preventDefault();
+			// add list item to list
+			reviewsList.appendChild(reviewBox);
+		}
+	});
 
-    // get form data
-    const formData = new FormData(addReviewForm);
+	// clear form fields
+	addReviewForm.reset();
 
-    // check form data
-    let valid = true;
-    if (!formData.get("review") || !formData.get("rating")) {
-        alert("Fields cannot be empty! Please try again.");
-        valid = false;
-    }
-    if (formData.get("rating") < '1' || formData.get("rating") > '5') {
-        alert("Rating must be a numeric value between 1 and 5! Please try again.");
-        valid = false;
-    }
+	// get avg rating without insignificant zeros
+	avg = +(avg / localStorage.length).toFixed(2);
+	if (localStorage.length) avgRating.innerHTML = 'Average rating: ' + avg.toString() + '/5';
+};
 
-    // add review to localStorage
-    if (valid) {
-        localStorage.setItem(localStorage.length,
-            JSON.stringify({
-                "text": formData.get("review"),
-                "score": formData.get("rating") 
-            })
-        )
-    }
+addReviewForm.addEventListener('submit', (event) => {
+	// dont reload on sumbit
+	event.preventDefault();
 
-    localStorage.setItem(formData.get("review"), formData.get("rating"))
+	// get form data
+	const formData = new FormData(addReviewForm);
 
-    // console.log(localStorage)
+	// check form data
+	let valid = true;
+	if (!formData.get('review') || !formData.get('rating')) {
+		alert('Fields cannot be empty! Please try again.');
+		valid = false;
+	}
+	if (formData.get('rating') < '1' || formData.get('rating') > '5') {
+		alert('Rating must be a numeric value between 1 and 5! Please try again.');
+		valid = false;
+	}
 
-    renderData();
+	// add review to localStorage
+	if (valid) {
+		localStorage.setItem(
+			localStorage.length,
+			JSON.stringify({
+				text: formData.get('review'),
+				score: formData.get('rating'),
+			})
+		);
+	}
+
+	renderData();
 });
 
 renderData();
