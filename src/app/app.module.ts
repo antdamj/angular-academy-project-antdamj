6d1b4229d9f4e5563a723b2a5e69b3031fb53249
antdamj <antonio.damjanovic.8@gmail.com>
2021-07-09 02:33:50 +0200

    Add main layout component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { MatCardModule } from '@angular/material/card';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainLayoutComponent } from './main-layout/main-layout.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { AllShowsContainerComponent } from './all-shows-container/all-shows-container.component';
 
 @NgModule({
-	declarations: [AppComponent],
-	imports: [BrowserModule, BrowserAnimationsModule, MatCardModule],
+	declarations: [AppComponent, MainLayoutComponent, SidenavComponent, AllShowsContainerComponent],
+	imports: [BrowserModule, BrowserAnimationsModule, MatCardModule, MatSidenavModule],
 	providers: [],
 	bootstrap: [AppComponent],
 })
