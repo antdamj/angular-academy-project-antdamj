4a0dc2986da9c5b3c6f0a115a6d93f74bdbb74b7
antdamj <antonio.damjanovic.8@gmail.com>
2021-07-12 11:56:32 +0200

    Console log all shows rating percentages

--- src/app/all-shows-container/all-shows-container.component.ts
+++ src/app/all-shows-container/all-shows-container.component.ts
@@ -7,6 +7,10 @@ import { Show } from 'src/services/show.model';
 	styleUrls: ['./all-shows-container.component.scss'],
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AllShowsContainerComponent {
+export class AllShowsContainerComponent implements OnInit {
 	@Input() shows: Array<Show>;
+
+	ngOnInit() {
+		this.shows.forEach((s) => console.log(s.getRatingPercentage()));
+	}
 }
