c02b81acb88c4cf2522b0b21460cc114ad971f4c
antdamj <antonio.damjanovic.8@gmail.com>
2021-07-21 14:30:07 +0200

    Create authentication service

---
+++ src/services/auth.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+	providedIn: 'root',
+})
+export class AuthService {
+	public registerUser(): void {
+		// make registration req
+	}
+
+	public loginUser(): void {
+		// make login req
+	}
+}
