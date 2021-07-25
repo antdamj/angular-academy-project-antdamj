c31e6992cd2f39201c2700cf5e81f19580372c68
antdamj <antonio.damjanovic.8@gmail.com>
2021-07-25 11:24:43 +0200

    Display newly added review without reloading the page

--- src/app/pages/add-review-container/components/add-review/add-review.component.ts
+++ src/app/pages/add-review-container/components/add-review/add-review.component.ts
@@ -1,5 +1,14 @@
-import { Component, ChangeDetectionStrategy, Input, Output, EventEmitter } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, NgControl, Validators } from '@angular/forms';
+import {
+	Component,
+	ChangeDetectionStrategy,
+	Input,
+	Output,
+	EventEmitter,
+	ComponentFactoryResolver,
+	ViewContainerRef,
+} from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ShowReviewComponent } from 'src/app/pages/show-details-container/components/show-details-card/components/show-review/show-review.component';
 
 export interface IAddForm {
 	rating: number;
@@ -14,7 +23,7 @@ export interface IAddForm {
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AddReviewComponent {
-	constructor(private fb: FormBuilder) {}
+	constructor(private fb: FormBuilder, private resolver: ComponentFactoryResolver, private vcref: ViewContainerRef) {}
 	@Input() showId: string;
 	@Output() formData: EventEmitter<IAddForm> = new EventEmitter();
 
@@ -24,7 +33,21 @@ export class AddReviewComponent {
 	});
 
 	public onAddReview(): void {
-		this.addReviewFormGroup.addControl('show_id', new FormControl(parseInt(this.showId)));
+		const componentFactory = this.resolver.resolveComponentFactory(ShowReviewComponent);
+		const viewContainerRef = this.vcref;
+
+		const componentRef = viewContainerRef.createComponent<ShowReviewComponent>(componentFactory);
+		componentRef.instance.review = {
+			rating: this.addReviewFormGroup.get('rating')?.value,
+			comment: this.addReviewFormGroup.get('comment')?.value,
+			id: '',
+			show_id: this.showId,
+		};
+
+		this.addReviewFormGroup.get('show_id')
+			? this.addReviewFormGroup.patchValue({ show_id: parseInt(this.showId) })
+			: this.addReviewFormGroup.addControl('show_id', new FormControl(parseInt(this.showId)));
+
 		this.formData.emit(this.addReviewFormGroup.value);
 		this.addReviewFormGroup.reset();
 	}
