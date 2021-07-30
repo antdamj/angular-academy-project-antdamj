fbb982ab9548a6f1a419ec7c00a1e3c7325c77e8
antdamj <antonio.damjanovic.8@gmail.com>
2021-07-30 11:10:59 +0200

    Modify interceptors

--- src/app/interceptors/auth-error.interceptor.ts
+++ src/app/interceptors/auth-error.interceptor.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor, HttpErrorResponse } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
+import { EMPTY, Observable, throwError } from 'rxjs';
 import { Router } from '@angular/router';
 import { catchError } from 'rxjs/operators';
 
@@ -12,7 +12,7 @@ export class AuthErrorInterceptor implements HttpInterceptor {
 		return next.handle(request).pipe(
 			catchError((error: HttpErrorResponse) => {
 				if (error.status === 401 || error.status === 403) {
-					this.router.navigate(['/login']);
+					EMPTY;
 				}
 				return throwError(error);
 			})
